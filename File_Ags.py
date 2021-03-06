fd4f9879ab73ee609de45a6b9d065ee209d10346
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-06 12:57:48 +0300

    Create File_Ags.py

---
+++ File_Ags.py
@@ -0,0 +1,28 @@
+# __Author__ __Lencof__
+# File_Ags.py
+
+Ags = '''
+Ags surprise
+'''
+
+# Open for 'w'riting
+f = opne('Ags.txt', 'w') # name file
+# Write text to file
+f.write(Ags)
+# Close the file
+f.close()
+
+# if no mode is specified,
+# 'r'ead mode is assumed by default
+f = open('Ags.txt') # name file
+while True: # use True
+    line = f.readline()
+    # Zero length indicates EOF
+    if len(line) == 0:
+        break
+    # The `line` already has a newline
+    # at the end of each line
+    # since it is reading from a file.
+    print(line, end='')
+# close the file
+f.close() # close()
