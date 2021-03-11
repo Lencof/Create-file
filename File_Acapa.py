e7cda791433d07a2810beb43fd6e326aba20b8f3
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-11 17:51:21 +0300

    Create File_Acapa.py

---
+++ File_Acapa.py
@@ -0,0 +1,28 @@
+# __Author__ __Lencof__
+# File_Acapa.py
+
+Acapa = '''
+Acapa
+'''
+
+# Open for 'w'riting
+f = opne('Acapa.txt', 'w') # name file
+# Write text to file
+f.write(Acapa)
+# Close the file
+f.close()
+
+# if no mode is specified,
+# 'r'ead mode is assumed by default
+f = open('Acapa.txt') # name file
+while True: # use True
+    line = f.readline()
+    # Zero length indicates EOF
+    if len(line) == 0:
+        break
+    # The `line` already has a newline
+    # at the end of each line
+    # since it is reading from a file.
+    print(line, end='')
+# close the file
+f.close() # close()
