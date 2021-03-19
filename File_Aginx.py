653ac980b81eb0a8fc3382c6a7672f08f089f25a
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-19 03:19:31 +0300

    Create File_Aginx.py

---
+++ File_Aginx.py
@@ -0,0 +1,28 @@
+# __Author__ __Lencof__
+# File_Aginx.py
+
+Aginx = '''
+Aginx surprise
+'''
+
+# Open for 'w'riting
+f = open('Aginx.txt', 'w') # name file
+# Write text to file
+f.write(Aginx)
+# Close the file
+f.close()
+
+# if no mode is specified,
+# 'r'ead mode is assumed by default
+f = open('Aginx.txt') # name file
+while True: # use True
+    line = f.readline()
+    # Zero length indicates EOF
+    if len(line) == 0:
+        break
+    # The `line` already has a newline
+    # at the end of each line
+    # since it is reading from a file.
+    print(line, end='')
+# close the file
+f.close() # close()
