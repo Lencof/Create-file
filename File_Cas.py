2811c44fa340a56118bff11c0d569f5e91e1fbbe
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-26 07:56:17 +0300

    Create File_Cas.py

---
+++ File_Cas.py
@@ -0,0 +1,28 @@
+# __Author__ __Lencof__
+# File_Cas.py
+
+Cas = '''
+Cas
+'''
+
+# Open for 'w'riting
+f = open('Cas.txt', 'w') # name file
+# Write text to file
+f.write(Cas)
+# Close the file
+f.close()
+
+# if no mode is specified,
+# 'r'ead mode is assumed by default
+f = open('Cas.txt') # name file
+while True: # use True
+    line = f.readline()
+    # Zero length indicates EOF
+    if len(line) == 0:
+        break
+    # The `line` already has a newline
+    # at the end of each line
+    # since it is reading from a file.
+    print(line, end='')
+# close the file
+f.close() # close()
