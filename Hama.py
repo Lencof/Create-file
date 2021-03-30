50275fbe05bb00a3e0d81f45b53ff7b47a3bc7e5
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-30 07:12:08 +0300

    Create Hama.py

---
+++ Hama.py
@@ -0,0 +1,30 @@
+# __Author__ __Lencof__
+# create_file.py
+
+# file name Hama
+# text description
+Hama = '''
+Hama
+'''
+
+# Open for 'w'riting
+f = opne('Hama.txt', 'w') # name file
+# Write text to file
+f.write(Hama)
+# Close the file
+f.close()
+
+# if no mode is specified,
+# 'r'ead mode is assumed by default
+f = open('Hama.txt') # name file
+while True: # use True
+    line = f.readline()
+    # Zero length indicates EOF
+    if len(line) == 0:
+        break
+    # The `line` already has a newline
+    # at the end of each line
+    # since it is reading from a file.
+    print(line, end='')
+# close the file
+f.close() 
